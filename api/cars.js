3b4ae611ddd8e5dc4364f2b008994ce87c789288
EwaSveta <ewasveta@gmail.com>
2023-06-21 04:46:18 +0300

    Car rent

---
+++ api/cars.js
@@ -0,0 +1,141 @@
+const express = require('express');
+const router = express.Router();
+const fs = require("fs")
+
+let cars = require("../cars.json")
+let { body, validationResult } = require('express-validator')
+
+
+// GET /api/cars - get all cars
+// local middleware
+router.get('/cars', (req, res) => 
+{
+    fs.readFile('./cars.json', (err, data) => {
+
+        cars = JSON.parse(data)
+
+        res.json(cars)
+    })
+})
+
+// GET /api/cars/1 - get car by id
+router.get('/cars/:id', (req, res) => 
+{
+    const id = req.params.id
+    const car = cars.find(car => car.id === +id)
+
+    if (!car)
+        return res.status(404).json({ "message": "car was not found" })
+
+    return res.json(car)
+})
+
+// POST /api/cars - create new car
+router.post('/cars', body('brand').notEmpty(), body('model').notEmpty(),
+        body('photo').notEmpty(), body('edition').notEmpty(), 
+        body('rent').notEmpty(), (req, res) => 
+{
+    const result = validationResult(req);
+
+    if (result.isEmpty()) {
+        const id = cars.length + 1
+        const car = { id, ...req.body }
+
+        console.log(req.body)
+
+        cars.push(car)
+
+        fs.writeFile('./cars.json', JSON.stringify(cars), (err) => 
+        {
+
+            if (err)
+                return res.status(400).json({ message: "Could not create a car" })
+
+            return res.status(201).json(car)
+        })
+    }
+    else
+    {
+        console.log(result.array())
+        // return res.status(400).json({ "message": "No car added" })
+        return res.status(400).json({ "errors": [`The ${result.array()[0].path} field is empty`] })
+    }
+})
+
+// PUT/PATCH /api/cars/1 - update specific car by id
+router.put('/cars/:id', (req, res) => 
+{
+    const carId = req.params.id
+    const newDataForCar = { ...req.body }
+    console.log(cars)
+
+    let car = cars.find(car => car.id === +carId)
+
+    const { id, ...other } = car
+
+    console.log(other)
+
+    if (req.body.brand !== "" && req.body.brand !== undefined && typeof req.body.brand === "String") {
+        // update the json
+        console.log("update = put")
+    }
+
+    console.log({ id, newDataForCar })
+
+    res.json({ message: "Sababa" })
+
+})
+
+// DELETE /api/cars/1 - delete specific car by id
+router.delete('/cars/:id', (req, res) => 
+{
+    const carId = req.params.id
+    console.log(carId)
+
+    let theCar = cars.find(car => car.id === +carId)
+
+    let ndx = cars.indexOf(theCar)
+    if (ndx > -1) 
+    { 
+        cars.splice(ndx, 1);
+    
+        fs.writeFile('./cars.json', JSON.stringify(cars), (err) =>
+        {
+            if (err)
+                return res.status(400).json({ message: "Could not remove a car" })
+
+            return res.status(201).json(theCar)
+        })
+    }
+    else
+    {
+        return res.status(400).json({ message: "Wrong car ID" })
+    }
+})
+
+module.exports = router;
+
+
+
+
+    // const result = validationResult(req);
+    // if (result.isEmpty()) {
+    //     const data = matchedData(req);
+    //     return res.send(`Hello, ${data.person}!`);
+    // }
+
+    // if(!car)
+    //   return res.status(400).json({"message":"No car added"})
+
+    // cars.push(car)
+
+    // fs.writeFile('./cars.json', JSON.stringify(cars) ,(err) => {
+
+    //   if(err)
+    //     return res.status(400).json({message: "Could not create a car"})
+
+    //   res.status(201).json(car)
+    // })
+    // fs.appendFile()
+
+    // res.json({message: "car successfully added"})
---
+++ client/script.js
@@ -0,0 +1,140 @@
+// Client Side!!!
+// [{"id":1,"brand":"BMW"},{"id":2,"brand":"Audi"},{"id":3,"brand":"Tesla"},{"id":4,"brand":"Toyota"},{"id":5,"brand":"Skoda"},{"id":7,"brand":"Volvo"},{"id":8,"brand":"Lamborghini"},{"id":9,"brand":"Renault"},{"id":9,"brand":"Peugeot"},{"id":10,"brand":"Honda"}]
+//Reventón
+
+// console.log("From the frontend")
+// alert("From the frontend")
+
+//const carsList = document.querySelector("ul#cars-list")
+const carsCards = document.querySelector("#crds")
+
+function showCars(){
+    fetch("http://localhost:3000/api/cars")
+    .then(res => res.json())
+    .then(data => {
+
+        // data.forEach(car => {
+        //     carsList.innerHTML += `
+        //     <li>${car.brand}</li>
+        //  
+        
+        data.forEach(car => {
+            carsCards.innerHTML += `
+            <div class="col-sm-6 mb-3">
+            <div class="card">
+              <img id="i${car.id}" class="card-img-top" src="${car.photo}" alt="${car.brand} image">
+              <div class="card-body">
+                <h5 id="h${car.id}" class="card-title">${car.brand} ${car.model} ${car.edition}</h5>
+                <p id="p${car.id}" class="card-text">Daily rent = ${car.rent} &#8362;</p>
+                <a href="#" onclick="del(${car.id})" class="btn btn-outline-secondary">Delete</a>
+                <a href="#" onclick="upd(${car.id})" class="btn btn-outline-secondary">Update</a>
+              </div>
+            </div>
+          </div>
+            `
+        });
+    })
+}
+showCars()
+
+function handleSubmit(e)
+{
+    e.preventDefault()
+    console.log("sending...")
+     
+    const brand   = e.target[0].value 
+    const model   = e.target[1].value 
+    const photo   = e.target[2].value 
+    const edition = e.target[3].value 
+    const rent    = e.target[4].value 
+
+    const data = {brand, model, photo, edition, rent}
+
+    fetch("http://localhost:3000/api/cars",
+    {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            // 'Content-Type': 'application/x-www-form-urlencoded',
+        },
+        body: JSON.stringify(data)
+    })
+    .then(res => res.json())
+    .then(resData => 
+        {
+        if(resData.errors?.length > 0)
+        {
+            const alert = document.querySelector("#alert")
+            
+            alert.className = "alert alert-danger"
+            alert.role = "alert"
+
+            for (const err of resData.errors) {
+                alert.innerHTML +=  err+ "<br/>"
+            }
+        }
+    })
+
+    console.log("here submit")
+    
+    toggle(false)
+    // fetch("http://localhost:3000/api/cars/7",{
+    //     method: "PUT",
+    //     data: JSON.stringify({brand: "Tesla"})
+    // })
+    // .then(res => res.json())
+    // .then(data => console.log(data))
+}
+
+function del(id)
+{
+    console.log("deleting...")
+    fetch("http://localhost:3000/api/cars/" + id,
+    {
+        method: "DELETE",
+        headers: {
+            "Content-Type": "application/json",
+            //'Content-Type': 'application/x-www-form-urlencoded',
+        }
+    })
+    .then(res => res.json())
+    .then(resData => {
+        if(resData.errors?.length > 0)
+        {
+            const alert = document.querySelector("#alert")
+            
+            alert.className = "alert alert-danger"
+            alert.role = "alert"
+
+            for (const err of resData.errors) {
+                alert.innerHTML +=  err+ "<br/>"
+            }
+        }
+    })    
+}
+
+function upd(id)
+{
+    console.log("updating...")
+
+    toggle(true)
+
+    let h = document.querySelector(`#h${id}`).innerText.split(' ')   
+    document.querySelector("#BN").value = h[0]
+    document.querySelector("#MN").value = h[1]
+    document.querySelector("#MY").value = h[2]
+
+    let p = document.querySelector(`#p${id}`).innerText.split(" = ")[1].split(' ')[0]
+    document.querySelector("#DR").value = p
+
+    let i = document.querySelector(`#i${id}`).src
+    document.querySelector("#PL").value = i
+
+    del(id)
+}
+
+function toggle(bool)
+{    
+    document.querySelector("form").style.display = 
+                        bool  ?  "block" : "none"
+}
